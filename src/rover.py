2cc0a4b0063bce0e7bd9bce8d354002114857bf3
NathanLM <nathan.lemerrer@orange.fr>
2020-10-20 19:48:48 +0200

    Added rover commands

--- src/rover.py
+++ src/rover.py
@@ -3,7 +3,41 @@ from .planet import Planet
 
 class Rover:
 
-    def __init__(self, x, y, orientation):
+    global commands
+    global vectors
+
+    def __init__(self, x, y, orientation, planet):
         self.x = x
         self.y = y
         self.orientation = orientation
+        self.planet = planet
+
+    # Movement functions
+    def moveForward(self, vector):
+        pass
+
+    def moveBackward(self, vector):
+        pass
+
+    def moveLeft(self, vector):
+        pass
+
+    def moveRight(self, vector):
+        pass
+
+    # Rover commands
+    commands = {
+        'f': moveForward,
+        'b': moveBackward,
+        'l': moveLeft,
+        'r': moveRight
+    }
+
+    # Movement vectors
+    # Current orientation: [Distance x, Distance Y, Left, Right]
+    vectors = {
+        'N': [+0, +1, 'W', 'E'],
+        'E': [+1, +0, 'N', 'S'],
+        'W': [-1, +0, 'S', 'N'],
+        'S': [+0, -1, 'E', 'W']
+    }
